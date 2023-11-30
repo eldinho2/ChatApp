1f0a1b6515fe139270a963e64704a7144dca2aea
Eldinho2 <viniciusfod@gmail.com>
2023-11-30 16:29:44 -0300

    feat: dependencias e importacao

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
 import { PrismaModule } from '../prisma/prisma.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UserModule],
+  imports: [PrismaModule, AuthModule, UsersModule],
 })
 export class AppModule {}
--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  app.use(cookieParser());
+
   await app.listen(3001);
 }
 bootstrap();
