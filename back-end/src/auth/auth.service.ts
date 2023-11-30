843bed5f64cebce82e409a02edaaa1d6ca39c52f
Eldinho2 <viniciusfod@gmail.com>
2023-11-30 13:25:16 -0300

    feat: cria signup default function

--- back-end/src/auth/auth.service.ts
+++ back-end/src/auth/auth.service.ts
@@ -1,18 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { AuthDto } from './dto/auth.dto';
+import { PrismaService } from 'prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  constructor() {}
+  constructor(private prisma: PrismaService) {}
 
-  async signin() {
-    return { message: 'Signin' };
+  async signup(dto: AuthDto) {
+    const { userName, email, password } = dto;
+    if (this.emailUserExists) {
+      throw new BadRequestException('Email ja está sendo ultilizado');
+    }
+    if (this.userNameExists) {
+      throw new BadRequestException('Usuario ja está sendo ultilizado');
+    }
+
+    const hashedPassword = await this.hashPassword(password);
+
+    await this.prisma.user.create({
+      data: {
+        userName,
+        email,
+        hashedPassword,
+      },
+    });
   }
 
-  async signup() {
-    return { message: 'Signup' };
+  async signin() {
+    return { message: 'signin' };
   }
 
   async signout() {
     return { message: 'Signout' };
+  }
+
+  async emailUserExists(email: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    return user ? true : false;
+  }
+
+  async userNameExists(userName: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        userName,
+      },
+    });
+    return user ? true : false;
+  }
+
+  async hashPassword(password: string) {
+    const saltOrRounds = 10;
+
+    return await bcrypt.hash(password, saltOrRounds);
   }
 }
