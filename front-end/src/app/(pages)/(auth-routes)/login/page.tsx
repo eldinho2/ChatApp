66fb0aaafebdf4be68674b6d87dda9a44a0e3602
Eldinho2 <viniciusfod@gmail.com>
2023-12-01 00:29:28 -0300

    feat: componente de login

--- front-end/src/app/(pages)/(auth-routes)/login/page.tsx
+++ front-end/src/app/(pages)/(auth-routes)/login/page.tsx
@@ -1,42 +1,131 @@
 "use client";
-import React, { SyntheticEvent, useEffect, useState } from "react";
-import Cookie from "js-cookie";
-import axios from "axios";
-import { useRouter } from "next/navigation";
-import Link from "next/link";
-import { signIn } from "next-auth/react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import Image from "next/image";
+
+type FormData = {
+  userName: string;
+  email: string;
+  password: string;
+};
+
+const schema = z.object({
+  userName: z
+    .string()
+    .min(4, { message: "usuario deve ter pelo menos 4 caracteres" })
+    .max(20, { message: "Usuario nao pode ser maior que 20 caracteres" }),
+  email: z.string().email({ message: "Email inválido" }),
+  password: z
+    .string()
+    .min(6, { message: "A senha deve ter pelo menos 8 caracteres" })
+    .max(20, { message: "A senha não pode ter mais que 20 caracteres" }),
+});
 
 export default function Login() {
-  const [token, setToken] = useState<string>("");
-  const [email, setEmail] = useState<string>("viniuhftegrdize@gmail.com")
-  const [password, setPassword] = useState<string>("534543")
+  const [formType, setFormType] = useState("register");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
 
-  const router = useRouter();
+  const onSubmit = (data: FormData) => console.log(data);
 
-  const handleLogin = async (event: SyntheticEvent) => {
-    event.preventDefault();
-    const result = await signIn("credentials", {
-      email,
-      password,
-      redirect: false,
-    });
-
-    if (result?.error) {
-      console.log(result);
-      return;
-    }
-
-    router.replace("/chat");
-  };
+  const toggleFormType = () =>
+    setFormType((prevType) => (prevType === "login" ? "register" : "login"));
 
   return (
-    <main className="bg-gray-800 h-screen w-screen flex gap-8">
-      <input type="text" value={"viniuhftegrdize@gmail.com"} name="" id="" />
-      <input type="text" value={"534543"} name="" id="" />
-      <button onClick={handleLogin}>Login</button>
-      <Link href="/chat">
-        <p>Chat</p>
-      </Link>
-    </main>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="flex flex-col items-center justify-center gap-20">
+        <Image
+          src="/PFAC Logo.jpg"
+          alt="Logo Play For A Cause"
+          width={100}
+          height={100}
+        />
+        {formType === "login" ? (
+          <h1 className="text-2xl font-bold">Bem-vindo De Volta</h1>
+        ) : (
+          <h1 className="text-2xl font-bold">Criar Conta</h1>
+        )}
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <div className="h-[400px] w-[400px]">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col space-y-4 p-8 animate-fade-in-down"
+          >
+            {formType === "register" && (
+              <>
+                <input
+                  {...register("userName")}
+                  placeholder="Nome de Usuario"
+                  className="p-2 rounded border border-gray-200 focus:border-[#10a37f] animate-fade-in-up focus:outline-none"
+                />
+                {errors.userName && (
+                  <p className="text-red-500 animate-fade-in-down">
+                    {errors.userName.message}
+                  </p>
+                )}
+              </>
+            )}
+            <input
+              {...register("email")}
+              placeholder="Email"
+              className="p-2 rounded border border-gray-200 focus:border-[#10a37f] focus:outline-none"
+            />
+            {errors.email && (
+              <p className="text-red-500 animate-fade-in-down">
+                {errors.email.message}
+              </p>
+            )}
+
+            <input
+              {...register("password")}
+              type="password"
+              placeholder="Password"
+              className="p-2 rounded border border-gray-200 focus:border-[#10a37f] focus:outline-none"
+            />
+            {errors.password && (
+              <p className="text-red-500 animate-fade-in-down">
+                {errors.password.message}
+              </p>
+            )}
+
+            <button
+              type="submit"
+              className="p-2 rounded text-white bg-[#10a37f] hover:bg-[#079474] transition duration-200"
+            >
+              Login
+            </button>
+            <div className="flex  justify-center items-center">
+              {formType === "login" ? (
+                <div className="flex gap-2 text-base">
+                  <h1>Ja tem uma conta?</h1>
+                  <span
+                    className="text-green-600 cursor-pointer"
+                    onClick={toggleFormType}
+                  >
+                    Login
+                  </span>
+                </div>
+              ) : (
+                <div className="flex gap-2 text-base">
+                  <h1>Ainda nao tem conta?</h1>
+                  <span
+                    className="text-green-600 cursor-pointer"
+                    onClick={toggleFormType}
+                  >
+                    Registrar
+                  </span>
+                </div>
+              )}
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
