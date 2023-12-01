061305e9b8ee7761e4c113a5dcbb53c5cec9c705
Eldinho2 <viniciusfod@gmail.com>
2023-11-30 23:25:26 -0300

    feat: componente de Profile

--- front-end/src/app/Components/Aside/Profile.tsx
+++ front-end/src/app/Components/Aside/Profile.tsx
@@ -1,10 +1,37 @@
-import React from "react";
+import LogoutButton from './LoggoutButton';
+import React from 'react';
+import { nextAuthOptions } from '@/app/api/auth/[...nextauth]/route';
+import { getServerSession } from 'next-auth';
+import jwt, { Secret } from 'jsonwebtoken';
 
-export default function Profile() {
+import { Session } from 'next-auth';
+
+interface ExtendedSession extends Session {
+  message: string;
+}
+
+const secret = process.env.JWT_SECRET;
+
+export default async function Profile() {
+  const session = await getServerSession(nextAuthOptions) as ExtendedSession;
+  
+  const getUserNameFromJWT = () => {
+    const token = session?.message;
+    
+
+    if (token) {
+      const decodedToken = jwt.verify(token, secret as Secret) as { userName: string };
+      return decodedToken.userName;
+    }
+
+    return 'Nome de Usuário Desconhecido';
+  };
+
   return (
-    <div className="flex gap-2">
+    <div className="flex gap-2 text-red-800">
       <span>Foto</span>
-      <span>Name</span>
+      <span>{getUserNameFromJWT()}</span>
+      <LogoutButton />
     </div>
   );
 }
