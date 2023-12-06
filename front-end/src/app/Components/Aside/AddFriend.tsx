fb1825f8aa972edd440ca02e00ded42ac20664ab
Eldinho2 <viniciusfod@gmail.com>
2023-12-05 23:41:18 -0300

    feat: componente aside addfriend

--- front-end/src/app/Components/Aside/AddFriend.tsx
+++ front-end/src/app/Components/Aside/AddFriend.tsx
@@ -1,16 +1,44 @@
-import React from 'react'
-import { IoPersonAddSharp } from "react-icons/io5";
+'use client'
 
+import React, { useState } from 'react'
+import { IoPersonAddSharp } from "react-icons/io5";
+import axios from 'axios';
+import {getUserNameFromJWT} from '@/app/utils/getUserNameFromJWT'
 
 export default function AddFriend() {
+  const [userName, setUserName] = useState('');
+
+  const handleSearchChange = (event:any) => {
+    setUserName(event.target.value);
+  }
+
+  const handleSearchFriend = async () => {
+    try {
+      await axios.get(`http://localhost:3002/users/${userName}`);
+      const friendName = await getUserNameFromJWT() 
+      handleAddFriend(friendName, userName);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const handleAddFriend = async (friendName: string, userName: string) => {
+    try {
+      await axios.post(`http://localhost:3002/users/Addfriend`, { friendName, userName });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
-    <div className='flex gap-2'>
+    <div className='flex flex-co gap-2 text-red-500'>
       <div>
         <IoPersonAddSharp/>
       </div>
       <div>
-        Add Friend
-        </div>
+        <input type="text" placeholder="Search users" value={userName} onChange={handleSearchChange} />
+        <button onClick={handleSearchFriend}>Add Friend</button>
+      </div>
     </div>
   )
 }
