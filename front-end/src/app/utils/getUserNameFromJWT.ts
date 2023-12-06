66f0dfec158a5d1238a52691814bf6418d055ab9
Eldinho2 <viniciusfod@gmail.com>
2023-12-06 01:10:41 -0300

    feat: adiciona funcao getUserNameFromJWT

---
+++ front-end/src/app/utils/getUserNameFromJWT.ts
@@ -0,0 +1,33 @@
+"use server"
+import { nextAuthOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
+import jwt, { Secret } from "jsonwebtoken";
+
+import { Session } from "next-auth";
+
+interface ExtendedSession extends Session {
+  user: {
+    name?: string | null | undefined;
+    message: string;
+  };
+}
+
+const secret = process.env.JWT_SECRET;
+
+  
+export const getUserNameFromJWT = async() => {
+  const session = (await getServerSession(nextAuthOptions)) as ExtendedSession;
+
+  if (session) {
+    const decoded = jwt.verify(session.user.message, secret as Secret);
+    
+
+    if (typeof decoded === "object" && "userName" in decoded) {
+      return decoded.userName
+    }
+
+    return 'erro';
+  }
+
+  return "Convidado";
+};
