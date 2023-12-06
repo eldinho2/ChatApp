d293bb0bbf86a9b7c1127f883fc3a3092508519e
Eldinho2 <viniciusfod@gmail.com>
2023-12-06 14:02:05 -0300

    feat: adiciona funcionalizades no addfriend

--- front-end/src/app/Components/Aside/AddFriend.tsx
+++ front-end/src/app/Components/Aside/AddFriend.tsx
@@ -5,6 +5,7 @@ import { IoPersonAddSharp } from "react-icons/io5";
 import axios from "axios";
 import { getUserNameFromJWT } from "@/app/utils/getUserNameFromJWT";
 import Image from "next/image";
+import LoadingSpinner from "@/app/utils/LoadingSpinner";
 import { z } from "zod";
 
 const userNameSchema = z
@@ -13,62 +14,68 @@ const userNameSchema = z
   .max(20, "usuário não pode ter mais de 20 caracteres");
 
 export default function AddFriend() {
-  const [userName, setUserName] = useState("");
-  const [error, setError] = useState("");
-
-  const handleAddFriend = async (friendName: string, userName: string) => {
-    try {
-      await axios.post(`http://localhost:3002/users/Addfriend`, {
-        friendName,
-        userName,
-      });
-    } catch (error: any) {
-      setError(error.response.data.message);
-    }
-  };
-
-  const handleSearchFriend = async () => {
-    try {
-      userNameSchema.parse(userName)
-      await axios.get(`http://localhost:3002/users/${userName}`);
-      const friendName = await getUserNameFromJWT();
-      handleAddFriend(friendName, userName);
-    } catch (error: any) {
-      if (error instanceof z.ZodError) {
-        setError(error.errors[0].message);
-      } else {
+    const [userName, setUserName] = useState("");
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+  
+    const handleAddFriend = async (friendName: string, userName: string) => {
+      try {
+        await axios.post(`http://localhost:3002/users/Addfriend`, {
+          friendName,
+          userName,
+        });
+      } catch (error: any) {
         setError(error.response.data.message);
       }
-    }
-  };
-
-  return (
-    <>
-      <div className="flex items-center gap-2 text-white">
-        <div>
+    };
+  
+    const handleSearchFriend = async () => {
+      setLoading(true);
+      setUserName('');
+      try {
+        userNameSchema.parse(userName)
+        await axios.get(`http://localhost:3002/users/${userName}`);
+        const friendName = await getUserNameFromJWT();
+        handleAddFriend(friendName, userName);
+      } catch (error: any) {
+        if (error instanceof z.ZodError) {
+          setError(error.errors[0].message);
+        } else {
+          setError(error.response.data.message);
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+  
+    return (
+      <>
+        <div className="flex items-center gap-2 text-white">
           <Image
             src="/PFAC Logo.jpg"
-            alt="logo"
+            alt="add friend"
             width={30}
             height={30}
             className="rounded-full"
           />
-        </div>
-          <div>
-              <input
-                type="text"
-                placeholder="Search"
-                className="bg-transparent border-b-2 border-white outline-none text-white w-40"
-                value={userName}
-                onChange={(e) => setUserName(e.target.value)}
-              />
-        </div>
-        <IoPersonAddSharp
-          onClick={handleSearchFriend}
-          className="cursor-pointer transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
+        <input
+          type="text"
+          placeholder="Search"
+          className="bg-transparent border-b-2 border-white outline-none text-white w-40"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+          onKeyDown={(e) => { e.key === 'Enter' && handleSearchFriend() }}
         />
-      </div>
-      {error && <p className="text-red-500">{error}</p>}
-    </>
-  );
-}
+          {loading ? (
+            <LoadingSpinner colorSpinner="black"/>
+          ) : (
+            <IoPersonAddSharp
+              onClick={handleSearchFriend}
+              className="cursor-pointer transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
+            />
+          )}
+        </div>
+        {error && <p className="text-red-500">{error}</p>}
+      </>
+    );
+  }
