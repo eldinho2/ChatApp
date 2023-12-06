0fae63aefa0240d0bdf4ecffb08d843fd045ac6a
Eldinho2 <viniciusfod@gmail.com>
2023-12-06 14:02:27 -0300

    feat: aumenta width do asideBar

--- front-end/src/app/Components/Aside/AsideBar.tsx
+++ front-end/src/app/Components/Aside/AsideBar.tsx
@@ -5,7 +5,7 @@ import { Friends } from './index'
 
 export default function AsideBar() {
   return (
-    <aside className="bg-black text-white h-screen w-[230px] flex flex-col justify-evenly">
+    <aside className="bg-black text-white h-screen w-[260px] flex flex-col justify-evenly">
       <div>
         <AddFriend />
       </div>
