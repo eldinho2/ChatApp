cb763b68c1d6f55f761d9c9da842e7d45ce3019e
Eldinho2 <viniciusfod@gmail.com>
2023-12-06 01:12:16 -0300

    feat: import message no index

--- front-end/src/app/Components/MainContainer/index.ts
+++ front-end/src/app/Components/MainContainer/index.ts
@@ -2,5 +2,6 @@ import ChatContainer from './ChatContainer';
 import SendMessage from './SendMessage';
 import Chat from './Chat'
 import Header from './Header'
+import Message from './Message'
 
-export { ChatContainer, SendMessage, Chat, Header }
+export { ChatContainer, SendMessage, Chat, Header, Message }
