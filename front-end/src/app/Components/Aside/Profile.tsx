070b42bfddccddfbb5d8353a4f1d6457377e4c65
Eldinho2 <viniciusfod@gmail.com>
2023-12-05 23:42:03 -0300

    feat: componente aside profile

--- front-end/src/app/Components/Aside/Profile.tsx
+++ front-end/src/app/Components/Aside/Profile.tsx
@@ -2,12 +2,10 @@ import { LogoutButton, UserName } from "./index";
 import React from "react";
 
 export default function Profile() {
-  const key = Date.now();
-
   return (
     <div className="flex gap-2 text-red-800">
       <span>Foto</span>
-      <UserName key={key} />
+      <UserName />
       <LogoutButton />
     </div>
   );
