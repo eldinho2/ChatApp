a1aa2036f2c51eee818a222cb2f61499829b4be4
Eldinho2 <viniciusfod@gmail.com>
2023-12-06 01:05:55 -0300

    feat: migrations

--- back-end/prisma/migrations/20231205152001_init/migration.sql
+++ back-end/prisma/migrations/20231205152001_init/migration.sql
@@ -8,6 +8,16 @@ CREATE TABLE "User" (
     "updateAt" TIMESTAMP(3) NOT NULL
 );
 
+-- CreateTable
+CREATE TABLE "Friend" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "conversationId" TEXT NOT NULL,
+    "friendName" TEXT NOT NULL,
+
+    CONSTRAINT "Friend_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_id_key" ON "User"("id");
 
@@ -16,3 +26,6 @@ CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_userName_key" ON "User"("userName");
+
+-- AddForeignKey
+ALTER TABLE "Friend" ADD CONSTRAINT "Friend_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
