4158cd5dbf4615294f1568e389d0d5a1d030952f
Eldinho2 <viniciusfod@gmail.com>
2023-12-06 13:36:38 -0300

    feat: adiciona tratativas de erro no users service

--- back-end/src/users/users.service.ts
+++ back-end/src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
 import { v4 as uuidv4 } from 'uuid';
+import { NotFoundException, ConflictException } from '@nestjs/common';
 
 @Injectable()
 export class UserService {
@@ -21,111 +22,138 @@ export class UserService {
   }
 
   async getUser(userName: string) {
-    return this.prisma.user.findUnique({
-      where: {
-        userName: userName,
-      },
-      select: {
-        id: true,
-        userName: true,
-      },
-    });
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: {
+          userName: userName,
+        },
+        select: {
+          id: true,
+          userName: true,
+        },
+      });
+
+      if (!user) {
+        throw new NotFoundException('Usuario não encontrado');
+      }
+
+      return user;
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      } else {
+        console.error(error);
+        throw new Error('aconteceu um erro inesperado');
+      }
+    }
   }
 
   async getUserFriends(userName: string) {
-    return this.prisma.user.findUnique({
-      where: {
-        userName: userName,
-      },
-      select: {
-        id: true,
-        friends: true,
-      },
-    });
+    try {
+      return this.prisma.user.findUnique({
+        where: {
+          userName: userName,
+        },
+        select: {
+          id: true,
+          friends: true,
+        },
+      });
+    } catch (error) {
+      throw new Error('Usuario não encontrado');
+    }
   }
 
-  async addFriend(friendName: string, userName: string) {
-    const user = await this.prisma.user.findUnique({
-      where: { userName: userName },
-    });
+  async addFriend(userName: string, friendName: string) {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: { userName },
+        select: { id: true },
+      });
 
-    const friend = await this.prisma.user.findUnique({
-      where: { userName: friendName },
-    });
+      const friend = await this.prisma.user.findUnique({
+        where: { userName: friendName },
+        select: { userName: true },
+      });
 
-    if (!friend || !user) {
-      throw new Error('User or friend not found');
+      if (!friend || !user) {
+        throw new NotFoundException('Usuario ou amigo não encontrado');
+      }
+
+      const existingFriend = await this.prisma.friend.findFirst({
+        where: {
+          userId: user.id,
+          friendName: friend.userName,
+        },
+      });
+
+      if (existingFriend) {
+        throw new ConflictException('amigo ja adicionado');
+      }
+
+      const conversationId = uuidv4();
+
+      await this.prisma.friend.create({
+        data: {
+          userId: user.id,
+          friendName: friend.userName,
+          conversationId: conversationId,
+        },
+      });
+    } catch (error) {
+      if (
+        error instanceof NotFoundException ||
+        error instanceof ConflictException
+      ) {
+        throw error;
+      } else {
+        console.error(error);
+        throw new Error('ocorreu um erro inesperado');
+      }
     }
-
-    // Verifique se o amigo já existe na lista de amigos do usuário
-    const existingFriend = await this.prisma.friend.findFirst({
-      where: {
-        userId: user.id,
-        friendName: friend.userName,
-      },
-    });
-    if (existingFriend) {
-      throw new Error('Friend already added');
-    }
-    // Crie um novo conversationId
-    const conversationId = uuidv4();
-    // Adicione o user2 à lista de amigos do user1
-    await this.prisma.friend.create({
-      data: {
-        userId: user.id,
-        friendName: friend.userName,
-        conversationId: conversationId,
-      },
-    });
-    // Adicione o user1 à lista de amigos do user2
-    await this.prisma.friend.create({
-      data: {
-        userId: friend.id,
-        friendName: user.userName,
-        conversationId: conversationId,
-      },
-    });
-
-    return { message: 'Friend added successfully' };
   }
 
   async removeFriend(friendName: string, userName: string) {
-    const user = await this.prisma.user.findUnique({
-      where: {
-        userName: userName,
-      },
-      select: {
-        id: true,
-      },
-    });
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: {
+          userName: userName,
+        },
+        select: {
+          id: true,
+        },
+      });
 
-    const friend = await this.prisma.user.findUnique({
-      where: {
-        userName: friendName,
-      },
-      select: {
-        id: true,
-      },
-    });
+      const friend = await this.prisma.user.findUnique({
+        where: {
+          userName: friendName,
+        },
+        select: {
+          id: true,
+        },
+      });
 
-    if (!friend || !user) {
-      throw new Error('User or friend not found');
+      if (!friend || !user) {
+        throw new Error('Usuario ou amigo não encontrado');
+      }
+
+      // Remover o amigo da lista de amigos do usuário
+      await this.prisma.friend.deleteMany({
+        where: {
+          userId: user.id,
+          friendName: friendName,
+        },
+      });
+
+      // Remover o usuário da lista de amigos do amigo
+      await this.prisma.friend.deleteMany({
+        where: {
+          userId: friend.id,
+          friendName: userName,
+        },
+      });
+    } catch (error) {
+      throw new Error(error);
     }
-
-    // Remover o amigo da lista de amigos do usuário
-    await this.prisma.friend.deleteMany({
-      where: {
-        userId: user.id,
-        friendName: friendName,
-      },
-    });
-
-    // Remover o usuário da lista de amigos do amigo
-    await this.prisma.friend.deleteMany({
-      where: {
-        userId: friend.id,
-        friendName: userName,
-      },
-    });
   }
 }
