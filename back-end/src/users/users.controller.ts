c8a7e195b3a003ea004765ba2551bfe5c3e6e299
Eldinho2 <viniciusfod@gmail.com>
2023-12-06 01:09:22 -0300

    feat: adiciona rotas auziliares de usuarios  no user service

--- back-end/src/users/users.controller.ts
+++ back-end/src/users/users.controller.ts
@@ -1,7 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
 import { UserService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UserService) {}
+
+  @Get(':username')
+  getUser(@Param('username') username: string) {
+    return this.usersService.getUser(username);
+  }
+
+  @Post('/Addfriend')
+  addFriend(
+    @Body('friendName') friendName: string,
+    @Body('userName') userName: string,
+  ) {
+    return this.usersService.addFriend(friendName, userName);
+  }
+
+  @Post('/Dellfriend')
+  removeFriend(
+    @Body('friendName') friendName: string,
+    @Body('userName') userName: string,
+  ) {
+    return this.usersService.removeFriend(friendName, userName);
+  }
+
+  @Get(':username/friends')
+  getUserFriends(@Param('username') username: string) {
+    return this.usersService.getUserFriends(username);
+  }
 }
