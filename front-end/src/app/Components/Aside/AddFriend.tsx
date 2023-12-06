b3851b807412ead55f65b9df403358eae2a47499
Eldinho2 <viniciusfod@gmail.com>
2023-12-06 13:35:47 -0300

    feat: melhorias no add friend

--- front-end/src/app/Components/Aside/AddFriend.tsx
+++ front-end/src/app/Components/Aside/AddFriend.tsx
@@ -1,44 +1,74 @@
-'use client'
+"use client";
 
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { IoPersonAddSharp } from "react-icons/io5";
-import axios from 'axios';
-import {getUserNameFromJWT} from '@/app/utils/getUserNameFromJWT'
+import axios from "axios";
+import { getUserNameFromJWT } from "@/app/utils/getUserNameFromJWT";
+import Image from "next/image";
+import { z } from "zod";
+
+const userNameSchema = z
+  .string()
+  .min(4, "usuário deve ter pelo menos 4 caracteres")
+  .max(20, "usuário não pode ter mais de 20 caracteres");
 
 export default function AddFriend() {
-  const [userName, setUserName] = useState('');
-
-  const handleSearchChange = (event:any) => {
-    setUserName(event.target.value);
-  }
-
-  const handleSearchFriend = async () => {
-    try {
-      await axios.get(`http://localhost:3002/users/${userName}`);
-      const friendName = await getUserNameFromJWT() 
-      handleAddFriend(friendName, userName);
-    } catch (error) {
-      console.error(error);
-    }
-  }
+  const [userName, setUserName] = useState("");
+  const [error, setError] = useState("");
 
   const handleAddFriend = async (friendName: string, userName: string) => {
     try {
-      await axios.post(`http://localhost:3002/users/Addfriend`, { friendName, userName });
-    } catch (error) {
-      console.error(error);
+      await axios.post(`http://localhost:3002/users/Addfriend`, {
+        friendName,
+        userName,
+      });
+    } catch (error: any) {
+      setError(error.response.data.message);
     }
-  }
+  };
+
+  const handleSearchFriend = async () => {
+    try {
+      userNameSchema.parse(userName)
+      await axios.get(`http://localhost:3002/users/${userName}`);
+      const friendName = await getUserNameFromJWT();
+      handleAddFriend(friendName, userName);
+    } catch (error: any) {
+      if (error instanceof z.ZodError) {
+        setError(error.errors[0].message);
+      } else {
+        setError(error.response.data.message);
+      }
+    }
+  };
 
   return (
-    <div className='flex flex-co gap-2 text-red-500'>
-      <div>
-        <IoPersonAddSharp/>
+    <>
+      <div className="flex items-center gap-2 text-white">
+        <div>
+          <Image
+            src="/PFAC Logo.jpg"
+            alt="logo"
+            width={30}
+            height={30}
+            className="rounded-full"
+          />
+        </div>
+          <div>
+              <input
+                type="text"
+                placeholder="Search"
+                className="bg-transparent border-b-2 border-white outline-none text-white w-40"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+              />
+        </div>
+        <IoPersonAddSharp
+          onClick={handleSearchFriend}
+          className="cursor-pointer transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
+        />
       </div>
-      <div>
-        <input type="text" placeholder="Search users" value={userName} onChange={handleSearchChange} />
-        <button onClick={handleSearchFriend}>Add Friend</button>
-      </div>
-    </div>
-  )
+      {error && <p className="text-red-500">{error}</p>}
+    </>
+  );
 }
